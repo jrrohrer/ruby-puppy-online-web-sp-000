28d296b755627d454eba02e7c180bd7e2eb3c37b
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-09-30 17:32:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,7 +6,7 @@ class Dog
 
   def initialize(name)
     @name =  name
-    self.save
+    @@all << self
   end
 
   def self.all
@@ -24,6 +24,6 @@ class Dog
   end
 
   def self.save
-    @@all << self
+    
   end
 end
